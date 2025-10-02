140a1ebfef77e7d762ba21e9c8b9a336a8bad96e
ChoiMinSang <min393917@gmail.com>
2025-10-02 13:37:32 +0900

    initial commit

---
+++ src/main/java/carbon/carbon_be/CarbonBeApplication.java
@@ -0,0 +1,13 @@
+package carbon.carbon_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonBeApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(CarbonBeApplication.class, args);
+   }
+
+}
---
+++ src/test/java/carbon/carbon_be/CarbonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package carbon.carbon_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonBeApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
